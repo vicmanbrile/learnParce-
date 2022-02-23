d0e8ecc37272d3002f8d488046f1b53f10fa2bb8
vicmanbrile <victor.briseno.leon@hotmail.com.mx>
2022-02-22 23:25:23 -0600

    delete cache and add tsconfig file

--- dist/index.579125c3.js
+++
@@ -1,4 +0,0 @@
-const app = document.getElementById('app');
-app.innerHTML = "<h1>Hola Mundo, ¿Como estas?</h1>";
-
-//# sourceMappingURL=index.579125c3.js.map
--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,14 @@
+import * as React from 'react'
 import {render} from 'react-dom'
 
 import './styles.css'
 
-function App(){
-		return (
-		 	<h1>Hello Word</h1>
+class App extends React.Component{
+	render() {
+		return(
+			<h1>Hello Word</h1>
 		)
+	}
 }
 
 const app = document.getElementById('app')
